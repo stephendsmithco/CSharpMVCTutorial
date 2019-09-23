9bc3e1c7e4f145cb2b8b721b2d97e00a22d8be90
Stephen Smith <stephendsmithco@gmail.com>
2019-09-22 19:48:26 -0600

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpMVCTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "Main Hello World route";
+        }
+
+        public string Welcome()
+        {
+            return "Welcome action method";
+        }
+    }
+}
